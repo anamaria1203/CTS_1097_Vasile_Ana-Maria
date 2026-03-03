2c1fbf7b9b9c36127a7b0cb8338a37849bd37804
anamaria1203 <anamaria.vasile2005112@gmail.com>
2026-03-03 14:13:24 +0200

    Update Angajat.java

--- CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Angajat.java
+++ CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Angajat.java
@@ -5,6 +5,18 @@ import java.util.Arrays;
 public class Angajat extends Aplicant{
 	private String ocupatie;
 	private int salariu;
+
+
+	public Angajat() {
+		super();
+	}
+
+	public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte,int salariu,String ocupatie) {
+	 	super(nume,prenume,varsta,punctaj,nr_proiecte,denumire_Proiecte);
+	 	this.salariu = salariu;
+		this.ocupatie = ocupatie;
+	}
+
 	
 	public String getOcupatie() {
 		return ocupatie;
@@ -22,18 +34,6 @@ public class Angajat extends Aplicant{
 		this.salariu = salariu;
 	}
 	
-	public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte,int salariu,String ocupatie) {
-		super(nume,prenume,varsta,punctaj,nr_proiecte,denumire_Proiecte);
-		this.salariu = salariu;
-		this.ocupatie = ocupatie;
-	}
-	
-	public Angajat() {
-		super();
-		
-	}
-	
-	
 	@Override
 	public String toString() {
 		return "Angajat: Nume=" + nume + ", Prenume=" + prenume
@@ -41,10 +41,8 @@ public class Angajat extends Aplicant{
 				+ Arrays.toString(denumireProiect) + "Ocupatie=" + ocupatie + ", salariu=" + salariu;
 	}
 	
-	
 	public int finantare() {
-		int s=10;
-		// TODO Auto-generated method stub
+		int s=10ș
 		System.out.println("Angajatul "+getNume()+" "+getPrenume()+" primeste"+s+" Euro/zi in proiect.");
 		return s;
 	}
