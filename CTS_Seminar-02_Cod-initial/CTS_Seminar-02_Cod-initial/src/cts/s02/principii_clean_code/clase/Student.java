9b675b7d534fbb1d674dbabe10ed7f9690bf6e06
anamaria1203 <anamaria.vasile2005112@gmail.com>
2026-03-05 10:53:06 +0200

    Refactor Student class attributes and methods

--- CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Student.java
+++ CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Student.java
@@ -4,43 +4,44 @@ import java.util.Arrays;
 
 public class Student extends Aplicant {
 	protected String facultate;
-	protected int an_studii;
-	
-	
-	public String getFacultate() {
-		return facultate;
-	}
-	public void setFacultate(String facultate) {
-		this.facultate = facultate;
-	}
-	public int getAn_studii() {
-		return an_studii;
-	}
-	public void setAn_studii(int an_studii) {
-		this.an_studii = an_studii;
-	}
-
+	protected int anStudii;
+	private static int sumaFinantare = 10;
 
 	public Student() {
 		super();
-		
+
 	}
-	
-	public Student(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect, String facultate, int an_studii) {
-		super(nume,prenume,varsta,punctaj,nr_proiecte,denumireProiect);
+
+	public Student(String nume, String prenume, int varsta, int punctaj, int numarProiecte, String[] denumiriProiecte, String facultate, int anStudii) {
+		super(nume, prenume, varsta, punctaj, numarProiecte, denumiriProiecte);
 		this.facultate = facultate;
-		this.an_studii = an_studii;
+		this.anStudii = anStudii;
 	}
+
+	public String getFacultate() {
+		return facultate;
+	}
+
+	public void setFacultate(String facultate) {
+		this.facultate = facultate;
+	}
+
+	public int getAn_studii() {
+		return anStudii;
+	}
+
+	public void setAn_studii(int an_studii) {
+		this.anStudii = an_studii;
+	}
+
 	@Override
 	public String toString() {
-		return "Student: Nume=" + nume + ", Prenume=" + prenume + ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte +  ", DenumireProiect=" + Arrays.toString(denumireProiect) + "Facultate=" + facultate + ", An_studii=" + an_studii ;
+		return super.toString() + "Facultate= " + this.facultate + ", An studii=" + this.anStudii ;
 	}
-	
-	
-	public int finantare() {
-		int s=20;
-		System.out.println("Studentul "+getNume()+" "+getPrenume()+" primeste"+s+" Euro/zi in proiect.");
-		return s;
+
+	@Override
+	public void afisareFinantare() {
+		super.afisareFinantare(sumaFinantare, "Student");
 	}
 	
 }
