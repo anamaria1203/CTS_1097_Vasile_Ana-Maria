6b05352ca0d6de3e3c5723b7b108999b768b98b8
anamaria1203 <anamaria.vasile2005112@gmail.com>
2026-03-05 10:51:47 +0200

    Refactor Angajat class for improved readability

--- CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Angajat.java
+++ CTS_Seminar-02_Cod-initial/CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Angajat.java
@@ -5,46 +5,42 @@ import java.util.Arrays;
 public class Angajat extends Aplicant{
 	private String ocupatie;
 	private int salariu;
-
+	private static int sumaFinantare = 10;
 
 	public Angajat() {
 		super();
 	}
 
-	public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte,int salariu,String ocupatie) {
-	 	super(nume,prenume,varsta,punctaj,nr_proiecte,denumire_Proiecte);
-	 	this.salariu = salariu;
+	public Angajat(String nume, String prenume, int varsta, int punctaj, int numarProiecte, String[] denumiriProiecte, int salariu, String ocupatie) {
+		super(nume, prenume, varsta, punctaj, numarProiecte, denumiriProiecte);
+		this.salariu = salariu;
 		this.ocupatie = ocupatie;
 	}
 
-	
 	public String getOcupatie() {
-		return ocupatie;
+		return this.ocupatie;
 	}
-	
+
 	public void setOcupatie(String ocupatie) {
 		this.ocupatie = ocupatie;
 	}
-	
 	public int getSalariu() {
-		return salariu;
+		return this.salariu;
 	}
-	
 	public void setSalariu(int salariu) {
 		this.salariu = salariu;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Angajat: Nume=" + nume + ", Prenume=" + prenume
-				+ ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte + ", DenumireProiect="
-				+ Arrays.toString(denumireProiect) + "Ocupatie=" + ocupatie + ", salariu=" + salariu;
+		return "Angajat: Nume=" + this.nume + ", Prenume=" + this.prenume
+				+ ", Varsta=" + this.varsta + ", Punctaj=" + this.punctaj + ", Numar proiecte="
+				+ this. numarProiecte + ", Denumiri Proiecte="
+				+ Arrays.toString(this.denumiriProiecte) + "Ocupatie=" + this.ocupatie + ", salariu=" + this.salariu;
 	}
-	
-	public int finantare() {
-		int s=10ș
-		System.out.println("Angajatul "+getNume()+" "+getPrenume()+" primeste"+s+" Euro/zi in proiect.");
-		return s;
+
+	@Override
+	public void afisareFinantare(int sumaFinantare, String tipPersoana){
+		super.afisareFinantare(sumaFinantare, "Angajat");
 	}
-	
 }
