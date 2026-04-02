4def83aa8f9c2b2ed15617e7c7649b0128152297
Ana <anamaria.vasile2005112@gmail.com>
2026-04-02 20:03:33 +0300

    add files

---
+++ CTS-master/Seminar-06/src/factory/_01simplesingleton/MainSimpleFactorySingleton.java
@@ -0,0 +1,17 @@
+import factory._01simplesingleton.Supa;
+import factory._01simplesingleton.SupaSimpleFactorySingleton;
+import factory._01simplesingleton.TipSupa;
+
+import java.util.Arrays;
+import java.util.List;
+
+void main() {
+    SupaSimpleFactorySingleton supaSimpleFactorySingleton = SupaSimpleFactorySingleton.getInstanta();
+    Supa supaLegume = supaSimpleFactorySingleton.getSupa(TipSupa.LEGUME, "Supa de legume");
+    Supa supaCiuperci = supaSimpleFactorySingleton.getSupa(TipSupa.CIUPERCI, "Supa de ciuperci");
+    Supa supaVita = supaSimpleFactorySingleton.getSupa(TipSupa.VITA, "Supa de vita", 120.0);
+
+    List<Supa> supe = Arrays.asList(supaLegume, supaCiuperci, supaVita);
+    supe.forEach(Supa::afiseazaDetalii);
+    //lkk
+}
